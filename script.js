f0df385af4eed646bd2e42e468d487e8d8e2be52
Aimen Labidi <127218255+aimenjs4@users.noreply.github.com>
2024-06-06 11:23:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", function() {
+  let header = document.querySelector("header");
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});
+  window.addEventListener("scroll", () => {
+      header.classList.toggle("shadow", window.scrollY > 0);
+  });
 
-menu.onclick = () => {
-  navbar.classList.toggle("active");
-};
-window.onscroll = () => {
-  navbar.classList.remove("active");
-};
+  menu.onclick = () => {
+      navbar.classList.toggle("active");
+  };
+  window.onscroll = () => {
+      navbar.classList.remove("active");
+  };
 
-// Dark Mode / Light Mode
-let darkmode = document.querySelector("#darkmode");
+  // Dark Mode / Light Mode
+  let darkmode = document.querySelector("#darkmode");
 
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
+  darkmode.onclick = () => {
+      if (darkmode.classList.contains("bx-moon")) {
+          darkmode.classList.replace("bx-moon", "bx-sun");
+          document.body.classList.add("active");
+      } else {
+          darkmode.classList.replace("bx-sun", "bx-moon");
+          document.body.classList.remove("active");
+      }
+  };
+
+  // Typewriter Effect
+  const text = "Aimen Labidi";
+  const typewriterElement = document.getElementById("typewriter");
+  let i = 0;
+  const speed = 100; // The speed/duration of the effect in milliseconds
+
+  function typeWriter() {
+      if (i < text.length) {
+          typewriterElement.textContent += text.charAt(i);
+          i++;
+          setTimeout(typeWriter, speed);
+      }
   }
-};
+
+  typeWriter();
+});
